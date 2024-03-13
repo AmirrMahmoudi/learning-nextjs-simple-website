ee1305de6daf38906cca7b54391bacdd21950aaa
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-13 16:34:54 +0330

    Define routes

---
+++ src/app/(account)/student/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export default function StudentLayout({children}:{children:ReactNode}) {
+    
+  return (
+    <>
+    <aside className="bg-gray-300 w-80 flex justify-center items-center"></aside>
+    <main className="flex items-center">
+    {children}
+    </main>
+    </>
+  )
+}
