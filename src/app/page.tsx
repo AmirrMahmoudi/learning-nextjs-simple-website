26d4b6a7675234f4db39e080b0cc26407a65fbdd
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-27 14:08:47 +0330

    Connection with classbon server and fetch the latest courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,29 @@ import Image from "next/image";
 import { Colors } from "./_components/colors/colors";
 import { Button } from "./_components/button";
 import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
-export default function Home() {
+import { CourseSummary } from "@/types/course-summary.interface";
+
+async function getNewestCourses(count: number): Promise<CourseSummary[]> {
+  const res = await fetch(
+    `https://api.classbon.com/api/courses/newest/${count}`,
+    {
+      next: {
+        revalidate: 24 * 60 * 60,
+      },
+    }
+  );
+  return res.json();
+}
+
+export default async function Home() {
+  const newestCourses = await getNewestCourses(4);
+  console.log(newestCourses[0].title);
   return (
     <>
       <HomeHeroSection />
-      <div className="container">
-        <Button variant="info" size="large" shape="wide" animatedIcon={true}>
-          ثبت نام
-        </Button>
-      </div>
+      {newestCourses.map((p) => (
+        <p key={p.title}>{p.title}</p>
+      ))}
     </>
   );
 }
