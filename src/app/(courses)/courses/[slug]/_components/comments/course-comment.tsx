b4a3110426fe9161d5b2dcb0e07b62261beefe00
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-04-02 16:25:52 +0330

    add get course comments api

--- src/app/(courses)/courses/[slug]/_components/comments/course-comment.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comment.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import { readData } from "@/core/http-service/http-service";
+import { useParams } from "next/navigation";
 import { useEffect } from "react";
+import { useCourseComments } from "../../_api/get-comments";
 
 const CourseComments = () => {
-  useEffect(() => {
-    readData("/validation-error");
-  }, []);
-  return <></>;
+  const { slug } = useParams();
+  const { data: comments } = useCourseComments({
+    params: {
+      slug: slug as string,
+      page: 1,
+    },
+  });
+  return (
+    <>
+      {comments?.data.map((p) => (
+        <p className="mb-8" key={p.id}>
+          {p.commentText}
+        </p>
+      ))}
+    </>
+  );
 };
 
 export default CourseComments;
