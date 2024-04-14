b2ff1266bd5b44462197e59daf306103d5cc766e
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-04-14 12:00:05 +0330

    update

--- src/actions/auth/index.ts
+++ src/actions/auth/index.ts
@@ -1,8 +1,16 @@
 "use server";
 
+import { signInSchema } from "@/app/(auth)/signin/_types/signin.schema";
 import { redirect } from "next/navigation";
 
 export async function signInAction(mobile: string) {
-  console.log(mobile);
-  redirect("/");
+  const validatedData = signInSchema.safeParse({
+    mobile,
+  });
+  if (!validatedData.success) {
+    console.log("error");
+  } else {
+    console.log(mobile);
+    redirect("/");
+  }
 }
